b2839fb31c6a911d290cc18d86486628ed9c10e0
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2025-06-07 22:59:01 +0200

    update transitionbuffer

--- src/jymkit/algorithms/utils/_buffer.py
+++ src/jymkit/algorithms/utils/_buffer.py
@@ -9,8 +9,16 @@ from ._transition import Transition
 
 class TransitionBuffer(eqx.Module):
     """
-    A buffer for storing transitions.
-    This is a simple wrapper around a list of transitions.
+    A buffer for storing transitions. Samples uniformly from the buffer.
+    The buffer is implemented as a circular buffer, where the oldest transitions are overwritten
+    when the buffer is full.
+
+    **Arguments**:
+        `max_size`: The maximum size of the buffer.
+        `sample_batch_size`: The number of transitions to sample from the buffer.
+        `data_sample`: A sample `Transition` to initialize the buffer structure.
+        `num_batch_axes`: The number of batch axes in the transition data. Defaults to 2, which expects the first two axes
+            to be batch dimensions (e.g, (`rollout_length`, `num_envs`, ...))
     """
 
     data: Transition
@@ -18,6 +26,7 @@ class TransitionBuffer(eqx.Module):
     size: int
     max_size: int = eqx.field(static=True)
     sample_batch_size: int = eqx.field(static=True)
+    num_batch_axes: int = eqx.field(static=True, default=2)
 
     def __check_init__(self):
         assert self.sample_batch_size > 0, "sample_batch_size must be greater than 0"
@@ -26,14 +35,23 @@ class TransitionBuffer(eqx.Module):
             "sample_batch_size must be less than or equal to max_size"
         )
 
-    def __init__(self, max_size: int, sample_batch_size: int, data_sample: Transition):
+    def __init__(
+        self,
+        max_size: int,
+        sample_batch_size: int,
+        data_sample: Transition,
+        num_batch_axes: int = 2,
+    ):
         self.insert_position = 0
         self.max_size = max_size
         self.sample_batch_size = sample_batch_size
         self.size = 0
+        self.num_batch_axes = num_batch_axes
 
         self.data = jax.tree.map(
-            lambda x: jnp.zeros((self.max_size,) + x.shape[2:], dtype=x.dtype),
+            lambda x: jnp.zeros(
+                (self.max_size,) + x.shape[self.num_batch_axes :], dtype=x.dtype
+            ),
             data_sample,
         )
 
@@ -42,7 +60,7 @@ class TransitionBuffer(eqx.Module):
         Insert a transition into the buffer.
         """
         data = self.data
-        data_len = np.prod(jax.tree.leaves(transition)[0].shape[:2])
+        data_len = np.prod(jax.tree.leaves(transition)[0].shape[: self.num_batch_axes])
         insert_position = self.insert_position
 
         assert data_len < self.max_size, (
@@ -53,7 +71,7 @@ class TransitionBuffer(eqx.Module):
         idx = (jnp.arange(data_len) + insert_position) % self.max_size
         data = jax.tree.map(
             lambda x, y: x.at[idx].set(
-                y.reshape(-1, *y.shape[2:]),
+                y.reshape(-1, *y.shape[self.num_batch_axes :]),
                 unique_indices=True,
             ),
             data,
