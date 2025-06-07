33ec4fcdf5498c1610e7ad8d6f2c4a51617a7e98
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2025-06-07 21:38:16 +0200

    multi agent utils changes & refactor PPO

--- src/jymkit/algorithms/_algorithm.py
+++ src/jymkit/algorithms/_algorithm.py
@@ -5,13 +5,17 @@ from dataclasses import replace
 import equinox as eqx
 from jaxtyping import Array, Float, PRNGKeyArray, PyTree
 
-from jymkit import Environment, JumanjiWrapper, is_wrapped
+from jymkit import Environment, JumanjiWrapper, VecEnvWrapper, is_wrapped
 
 logger = logging.getLogger(__name__)
 
 
 class RLAlgorithm(eqx.Module):
     state: eqx.AbstractVar[PyTree[eqx.Module]]
+
+    @property
+    def is_initialized(self) -> bool:
+        return self.state is not None
 
     def save_state(self, file_path: str):
         with open(file_path, "wb") as f:
@@ -33,10 +37,16 @@ class RLAlgorithm(eqx.Module):
     ) -> Float[Array, " num_eval_episodes"]:
         pass
 
-    def __check_env__(self, env: Environment):
+    def __check_env__(self, env: Environment, vectorized: bool = False):
         if is_wrapped(env, JumanjiWrapper):
             logger.warning(
                 "Some Jumanji environments rely on specific action masking logic "
                 "that may not be compatible with this algorithm. "
                 "If this is the case, training will crash during compilation."
             )
+        if vectorized:
+            if not is_wrapped(env, VecEnvWrapper):
+                logger.info("Wrapping environment in VecEnvWrapper")
+                env = VecEnvWrapper(env)
+
+        return env
