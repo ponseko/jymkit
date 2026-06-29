360d8ca17b53bb740eed9bba0d52e7aa03260074
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2026-06-29 14:39:56 +0200

    remove networks submodule

--- src/jaxnasium/algorithms/networks/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._agent_networks import (
-    ActorNetwork as ActorNetwork,
-    AdvantageCriticNetwork as AdvantageCriticNetwork,
-    QValueNetwork as QValueNetwork,
-    ValueNetwork as ValueNetwork,
-)
