23c4b14d3e3b55c8ceb022f3d2cffd6018f1b91d
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2026-06-29 17:47:09 +0200

    fix bronet

--- src/jaxnasium/algorithms/_core/_architectures.py
+++ src/jaxnasium/algorithms/_core/_architectures.py
@@ -145,7 +145,7 @@ class BroNet(eqx.Module):
     Operates on 1D inputs.
     """
 
-    layers: List[eqx.Module]
+    layers: list
     in_features: int = eqx.field(static=True)
     width_size: int = eqx.field(static=True)
     depth: int = eqx.field(static=True)
@@ -182,7 +182,7 @@ class BroNet(eqx.Module):
         self.in_features = in_features
         self.width_size = width_size
         self.depth = depth
-        self.out_size = width_size
+        self.out_features = width_size
         self.layers = [
             eqx.nn.Linear(
                 in_features=in_features, out_features=width_size, key=keys[0]
@@ -191,3 +191,12 @@ class BroNet(eqx.Module):
         ]
         for i in range(1, depth + 1):
             self.layers.append(BroNetBlock(keys[i], width_size))
+
+    def __call__(self, x):
+        x = self.layers[0](x)  # dense
+        x = self.layers[1](x)  # layernorm
+        x = jax.nn.relu(x)
+        # then the bronet blocks:
+        for block in self.layers[2:]:
+            x = block(x)
+        return x
