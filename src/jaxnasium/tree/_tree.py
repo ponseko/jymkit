fc153f4ea04cda0621bab81770030a45d00ab769
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2026-07-15 18:37:39 +0200

    prevent deprecation warning

--- src/jaxnasium/tree/_tree.py
+++ src/jaxnasium/tree/_tree.py
@@ -74,7 +74,7 @@ def tree_map_distribution(fn: Callable, tree, *rest):
     try:
         import distrax
 
-        from jaxnasium.algorithms.utils import DistraxContainer
+        from jaxnasium.algorithms.core._distributions import DistraxContainer
     except ImportError:
         raise ImportError(
             "jaxnasium.algorithms is required for `jaxnasium.tree.map_distributions()`. Please install  `pip install jaxnasium[algs]`."
